3a170e71bd14bd5347902694eeb5a99d32d0ad64
Jeff Butler <jeffbutler25@gmail.com>
2020-11-03 07:47:00 -0500

    itsystems

---
+++ CarePartnersPortal/Data/ITSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarePartnersPortal.Data
+{
+    public class ITSystem
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ContactsInternal { get; set; }
+        public string ContactsExternal { get; set; }
+        public string NotificationGroup { get; set; }
+    }
+}
